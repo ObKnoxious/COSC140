027372da0444ef1097757525d77a4154be5342a6
Knox Crichton <kcrichto@vols.utk.edu>
2020-09-01 00:39:05 -0400

    Homework 1

---
+++ homework/hw_1b.cpp
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main(int argc, char* argv[]){
+	cout << "Num args = " << argc << "\n";
+	for(int i =0; i<argc; i++){
+		cout << "argv[" << i <<  "] = " <<  &argv[i] << " " << argv[i] << "\n";
+	}
+}
+
+
+int strlen(char *){
+	int r = 0;
+
+
+}	
