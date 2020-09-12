f546757c8bed490c527335cdefaabfc1cc2f586f
Knox Crichton <knoxcrichton@gmail.com>
2020-09-12 17:38:24 -0400

    Lab 2 awaiting testing

---
+++ homework/hw_4.cpp
@@ -0,0 +1,14 @@
+class List {
+	public:
+		stack();
+		empty();
+		push();
+		pop();
+		top();
+	private:
+		int size;
+		int *p;
+}
+
+// I don't know what error checking you need
+//
--- labs/lab2/Prog2a.cpp
+++ labs/lab2/Prog2a.cpp
@@ -39,15 +39,16 @@ int main(int argc, char *argv[])
 
 	// declare a table[][] that can keep track of the
 	// cards dealt for each suit -- initialize to 0
-	int w =0;
-	// Clubs, Hears, Spades, Diamonds 
+	// Clubs, Diamonds, Hearts, Spades 
 	int table[4][13] ={0};
-	while (w < 10) {
+	int sx = 4; // Stores which suit is the stop
+	while (true) {
 	  string card = random_card(verbose);
-		w++;
   		// I use hard tabs for everything and I'm not going to bother
 		// fixing this. It really do be that way tho
-		cout << card << " " << card[5] << " " << int(card[0]) << "\n";
+	//	if(verbose){
+	//		cout << card << "\n";
+	//	}
 		// Check if not face card
 		// Equivelant to !isAlpha()
 		if(int(card[0] < 65 && card[1] != '0')){
@@ -57,19 +58,19 @@ int main(int argc, char *argv[])
 			switch(card[5]){
 				case 'C':
 					table[0][v-1]=table[0][v-1] +1;
-					cout << table[0][v-1] << " ";
-					break;
-				case 'H':
-					table[1][v-1]+=1;
-					cout << table[1][v-1] << " ";
-					break;
-				case 'S':
-					table[2][v-1]+=1;
-					cout << table[2][v-1] << " ";
+			//		cout << table[0][v-1] << " ";
 					break;
 				case 'D':
+					table[1][v-1]+=1;
+			//		cout << table[1][v-1] << " ";
+					break;
+				case 'H':
+					table[2][v-1]+=1;
+		//			cout << table[2][v-1] << " ";
+			//		break;
+				case 'S':
 					table[3][v-1]+=1;
-					cout << table[3][v-1]<< " ";
+			//		cout << table[3][v-1]<< " ";
 					break;
 				default:
 					cout << "Please panic\n";
@@ -82,32 +83,167 @@ int main(int argc, char *argv[])
 			switch(card[6]){
 				case 'C':
 					table[0][v-1]=table[0][v-1] +1;
-					cout << table[0][v-1] << " ";
-					break;
-				case 'H':
-					table[1][v-1]+=1;
-					cout << table[1][v-1] << " ";
-					break;
-				case 'S':
-					table[2][v-1]+=1;
-					cout << table[2][v-1] << " ";
+				//	cout << table[0][v-1] << " ";
 					break;
 				case 'D':
+					table[1][v-1]+=1;
+				//	cout << table[1][v-1] << " ";
+					break;
+				case 'H':
+					table[2][v-1]+=1;
+				//	cout << table[2][v-1] << " ";
+					break;
+				case 'S':
 					table[3][v-1]+=1;
-					cout << table[3][v-1]<< " ";
+				//	cout << table[3][v-1]<< " ";
+					break;
+				default:
+					cout << "Please panic\n";
+			}
+		}else if(card[0] == 'A'){
+			// For aces
+			// Ace of X
+			int v=1;
+			switch(card[7]){
+				case 'C':
+					table[0][v-1]=table[0][v-1] +1;
+				//	cout << table[0][v-1] << " ";
+					break;
+				case 'D':
+					table[1][v-1]+=1;
+				//	cout << table[1][v-1] << " ";
+					break;
+				case 'H':
+					table[2][v-1]+=1;
+				//	cout << table[2][v-1] << " ";
+					break;
+				case 'S':
+					table[3][v-1]+=1;
+				//	cout << table[3][v-1]<< " ";
+					break;
+				default:
+					cout << "Please panic\n";
+			}
+		}else if(card[0] == 'J'){
+			// For Jacks
+			// Jack of X
+			int v=11;
+			switch(card[8]){
+				case 'C':
+					table[0][v-1]=table[0][v-1] +1;
+				//	cout << table[0][v-1] << " ";
+					break;
+				case 'D':
+					table[1][v-1]+=1;
+				//	cout << table[1][v-1] << " ";
+					break;
+				case 'H':
+					table[2][v-1]+=1;
+				//	cout << table[2][v-1] << " ";
+					break;
+				case 'S':
+					table[3][v-1]+=1;
+				//	cout << table[3][v-1]<< " ";
+					break;
+				default:
+					cout << "Please panic\n";
+			}
+		}else if(card[0] == 'Q'){
+			// For queens
+			// Queen of X
+			
+			int v=12;
+			switch(card[9]){
+				case 'C':
+					table[0][v-1]=table[0][v-1] +1;
+				//	cout << table[0][v-1] << " ";
+					break;
+				case 'D':
+					table[1][v-1]+=1;
+				//	cout << table[1][v-1] << " ";
+					break;
+				case 'H':
+					table[2][v-1]+=1;
+				//	cout << table[2][v-1] << " ";
+					break;
+				case 'S':
+					table[3][v-1]+=1;
+				//	cout << table[3][v-1]<< " ";
+					break;
+				default:
+					cout << "Please panic\n";
+			}
+		}else if(card[0] == 'K'){
+			// For Kings
+			// King of X
+
+			int v=13;
+			switch(card[8]){
+				case 'C':
+					table[0][v-1]=table[0][v-1] +1;
+				//	cout << table[0][v-1] << " ";
+					break;
+				case 'D':
+					table[1][v-1]+=1;
+				//	cout << table[1][v-1] << " ";
+					break;
+				case 'H':
+					table[2][v-1]+=1;
+				//	cout << table[2][v-1] << " ";
+					break;
+				case 'S':
+					table[3][v-1]+=1;
+				//	cout << table[3][v-1]<< " ";
 					break;
 				default:
 					cout << "Please panic\n";
 			}
 		}
+				
 	  // reverse engineer card suit and face (value)
 	  // update accordingly: table[suit][value]++
-
-      // break out of loop if stopping criteria met
-	}
+	
+      // break out of loop if stopping criteria meti
+      		if((table[0][10] > 0 && table[0][11] > 0 && table[0][12] > 0)){
+			sx = 0;
+			break;
+		}else if(table[1][10] > 0 && table[1][11] > 0 && table[1][12] 
+				>0){
+			sx =1;
+			break;
+		}else if(table[2][10] > 0 && table[2][11] > 0 && table[2][12] 
+				> 0){
+			sx = 2;
+			break;
+		}else if(table[3][10] > 0 && table[3][11] > 0 && table[3][12]
+			       	> 0){
+			sx = 3;
+			break;
+		}
+	}// This is the bracket for end of while
 	for(int i =0; i < 4; i++){
+		switch(i){
+			case 0:
+				cout << "   Clubs :";
+				break;
+			case 1:
+				cout << "Diamonds :";
+				break;
+			case 2:
+				cout << "  Hearts :";
+				break;
+			case 3:
+				cout << "  Spades :";
+				break;
+			default:
+				cout << "Please Panic\n";
+				break;
+		}
 		for(int j =0; j < 13; j++){
-			cout << table[i][j] << "\t";
+			cout <<"   " << table[i][j];
+		}
+		if(i == sx){
+			cout << " **";
 		}
 		cout << "\n";
 	}
