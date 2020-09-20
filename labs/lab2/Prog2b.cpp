eba234010a4f0c979dcb72a10cf4d7d0dced40f8
Knox Crichton <kcrichto@vols.utk.edu>
2020-09-20 19:18:30 -0400

    Lab 2 B done at last

--- labs/lab2/Prog2b.cpp
+++ labs/lab2/Prog2b.cpp
@@ -22,77 +22,173 @@ class Node {
 		int data;
 		Node* next;
 };
-
 class List{
 	public:
 		List(){
 			head = NULL;
 		}
 		~List(){};
-		void insert(int v);
+		void insert(int v, bool &s);
 		void printList();
 		void remove(Node *n);
-		Node* ghead(){
-			return head;
-		}
+		void sort();
+		bool check(int v, Node* &t);
 	private:
 		Node* head;
 };
 
-void List::insert(int v){
+
+void List::insert(int v, bool &s){
+	bool hj = false; // has jack
+	bool hq = false; // has queen
+	bool hk = false; // has king
+	// Create node to be inserted
 	Node* nNode = new Node();
 	nNode->data =v;
 	nNode->next = NULL;
 	if(head == NULL){
+		// if list is empty this become the head
 		head =nNode;
 	}else{
 		Node* tmp = head; 
+		// Loops until end of existing list
 		while(tmp->next != NULL){
+			// Check for duplicate
+			if(tmp->data == v){
+//				cout << "Duplicate detected\n";
+				remove(tmp);
+				return;
+			}
 			tmp = tmp->next;
 		}
+		// Check for duplicate after while loop because
+		// loop stops at second to last tmp
+		if(tmp->data == v){
+//			cout << "Duplicate detected\n";
+			remove(tmp);
+			return;
+		}
+		// Then inserts
 		tmp->next = nNode;
 	}
+	// Looping thru list to check if all face cards
+	if(head != NULL){
+		Node* tmpc = head;
+		while(tmpc != NULL){
+			if(tmpc->data == 11){
+				hj = true;
+			}
+			if(tmpc->data == 12){
+				hq = true;
+			}
+			if(tmpc->data == 13){
+				hk = true;
+			}
+			// S in the sentinal of loop in main
+			if(hj && hq && hk){
+//				cout << "time to stop\n";
+				s = false;
+			}
+			tmpc = tmpc->next;
+		}
+	}
+//	if(head != NULL){
+//		Node* tmpc2 = head;
+//		while(tmpc2 != NULL){
+//			if(tmpc2->data == v){
+//				//cout << "Duplicate found!\n";
+//				remove(tmpc2);
+//				break;
+//			} 
+//		}
+//	}
 }
 void List::printList(){
+	bool hj = false;
+	bool hq = false;
+	bool hk = false;
 	if(head == NULL){
-		cout << "No values in list\n";
+		// Used on instance 23 when no clubs are dealt
+		cout << "\n";
 	} else {
 		Node* tmp = head;
+		// Change output value to face card value
 		while(tmp != NULL){
 			if(tmp->data == 1){
 				cout << "Ace ";
 			}else if(tmp->data == 11){
+				hj = true;
 				cout << "Jack ";
 			}else if(tmp->data == 12){
+				hq = true;
 				cout << "Queen ";
 			}else if(tmp->data == 13){
+				hk = true;
 				cout << "King ";
 			}else{
 				cout << tmp->data << " ";
 			}
 			tmp = tmp->next;
 		}
+		if(hj && hq && hk){
+			cout << "**";
+		}
 		cout << "\n";
 	}
 }
 void List::remove(Node *n){
+	//cout << "Inside remove function\n";
 	if(n == head){
+		//cout << "Inside of first if\n";
+		// Have to move next data to head
+		if(head->next == NULL){
+			head=NULL;
+			// Should revert back to blank list
+			return;
+		}
 		head->data = head->next->data;
 		n = head->next;
 		head->next = head->next->next;
 		free(n);
 		return;
 	}
+	//cout << "Made it past first if\n";
 	Node *prev = head;
-	while(prev->next != NULL && prev->next !=n)
+	while(prev->next != NULL && prev->next !=n){
+		//cout << "Inside while loop of remove function\n";
 		prev = prev->next;
+	}
 	if(prev->next == NULL){
-		cout << "\n Bade node to delete";
+
+		// Can't delete null node
+		//cout << "\n Bade node to delete";
 		return;
 	}
 	prev->next = prev->next->next;
 	free(n);
 	return;
+}
+
+void List::sort(){
+	Node* current = head;
+	Node* index = NULL;
+	if(head == NULL){
+		return;
+	}else{
+		while(current != NULL){
+			index = current->next;
+			while(index != NULL){
+				if(current->data > index->data){
+					int tmp;
+					tmp = current->data;
+					current->data = index->data;
+					index->data = tmp;
+				}
+				index = index->next;
+			}
+			current = current->next;
+		}
+	}
 }
 
 int main(int argc, char *argv[]){
@@ -107,16 +203,18 @@ int main(int argc, char *argv[]){
 			} else 
 				cout << "option " << argv[i] << " ignored\n";
 		}
+		// Sentinal value for loop
 		bool sent = true;
 		srand(seedvalue);
+		// TODO: Replace this with array of these, causes seg fault
 		List* hearts = new List();
 		List* clubs = new List();
 		List* spades = new List();
 		List* diamonds = new List();
-		int fn =0;
-		while(fn < 10){
+		while(sent){
 			int v = -1;
 			string c = random_card(verbose);
+			// Finding value from string, taken from last lab
 			switch(c[0]){
 				case 'A':
 					v=1;
@@ -130,33 +228,54 @@ int main(int argc, char *argv[]){
 				case 'K':
 					v=13;
 					break;
+				case '1':
+					v=10;
+					break;
 				default:
 					v=int(c[0])-48;
 					break;
 			}
+			// Find what suit to put things in
+			// The if !sent break was to fix a problem earlier
+			// and probably isn't needed but not gonna risk
+			// breaking anything at this point
 			if(c.find("Hearts") != string::npos){
-				Node* tmp = hearts->ghead();
-				while(tmp != NULL){
-					if(tmp->data = v){
-
-				hearts->insert(v);
+				hearts->insert(v, sent);
+				hearts->sort();
+				if(!sent){
+					break;
+				}
 			}else if(c.find("Clubs") != string::npos){
-				clubs->insert(v);
+				clubs->insert(v, sent);
+				clubs->sort();
+				if(!sent){
+					break;
+				}
 			}else if(c.find("Spades") != string::npos){
-				spades->insert(v);
+				spades->insert(v, sent);
+				spades->sort();
+				if(!sent){
+					break;
+				}
 			}else if(c.find("Diamonds") != string::npos){
-				diamonds->insert(v);
+				diamonds->insert(v, sent);
+				diamonds->sort();
+				if(!sent){
+					break;
+				}
 			}
-			sent = false;
-			fn++;
 		}
 		cout <<"   Clubs : ";
-		clubs ->printList();
+		clubs->sort();
+		clubs->printList();
 		cout <<"Diamonds : ";
+		diamonds->sort();
 		diamonds->printList();
-		cout <<"  Hearts : ";		
+		cout <<"  Hearts : ";
+		hearts->sort();		
 		hearts->printList();
 		cout <<"  Spades : ";
+		spades->sort();
 		spades->printList();
 		return 0;
 }
