fde8ad20ada9b3b37fed10c6d0349bd47391655e
Knox Crichton <knoxcrichton@gmail.com>
2020-10-13 14:53:15 -0400

    Hashtable class maybe working?

--- labs/lab4/hashtable_qp.cpp
+++ labs/lab4/hashtable_qp.cpp
@@ -40,5 +40,40 @@ void hash_table<Tkey>::insert<const Tkey &key){
 	}
 }
 
+template <typename Tkey>
+int hash_table<Tkey>::nextprime(int N) {
+	 int i = 2;
+	 while (i*i <= N) {
+	 if (N%i == 0) { N++; i = 1; }
+	 i++;
+	}
+	return max(3,N);
+}
 
-
+template <typename Tkey>
+int hash_table<Tkey>::qprobe(const Tkey &key) {
+	int index = hash(key);
+	int k = 0;
+	while (table[index] != Tkey() && table[index] != key) {
+		index += 2*(++k) - 1;
+		index = index % table.size();
+	}
+		return index;
+}
+template <typename Tkey>
+void hash_table<Tkey>::resize() {
+	 vector<Tkey> tmp_table;
+	 for (int i=0; i<(int)table.size(); i++) {
+		 if (table[i] != Tkey())
+		 tmp_table.push_back(table[i]);
+	 }
+	 int N = nextprime(2*table.size());
+	 table.assign(N, Tkey());
+	 num_inuse = 0;
+	 max_inuse = N/2;
+	 for (int i=0; i<(int)tmp_table.size(); i++) {
+		 Tkey &key = tmp_table[i];
+		 table[ qprobe(key) ] = key;
+		 num_inuse++;
+	 }
+}
